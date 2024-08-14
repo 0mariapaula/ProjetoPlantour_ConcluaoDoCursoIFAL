660acdd2b7b9fc77cd60fdd67f5021ca4420b004
0mariapaula <0mariapaulaferreira@gmail.com>
2024-08-14 16:33:17 -0300

    Arrumando 0.1

--- app/Explorar.js
+++ app/Explorar.js
@@ -4,36 +4,23 @@ import { useRouter } from 'expo-router';
 import BottomNavBar from './BottomNavBar';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { collection, getDocs } from "firebase/firestore";
-import { db } from './../firebaseConfig'; // ajuste o caminho conforme necessário
+import { db } from './../firebaseConfig';
 
 const Explorar = () => {
   const router = useRouter();
   const [modalVisible, setModalVisible] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
   const [configModalVisible, setConfigModalVisible] = useState(false);
   const [publicacoes, setPublicacoes] = useState([]);
 
   useEffect(() => {
-    const fetchPublicacoes = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, "publicacoes"));
-        const data = querySnapshot.docs.map(doc => doc.data());
-        setPublicacoes(data);
-      } catch (error) {
-        console.error('Erro ao buscar publicações:', error);
-      }
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, "publicacoes"));
+      const fetchedPublicacoes = querySnapshot.docs.map(doc => doc.data());
+      setPublicacoes(fetchedPublicacoes);
     };
 
-    fetchPublicacoes();
+    fetchData();
   }, []);
-
-  const openSearchModal = () => {
-    setModalVisible(true);
-  };
-
-  const closeSearchModal = () => {
-    setModalVisible(false);
-  };
 
   const openConfigModal = () => {
     setConfigModalVisible(true);
@@ -66,46 +53,32 @@ const Explorar = () => {
             <Text style={styles.buttonTextB}>Hoteis</Text>
           </TouchableOpacity>
 
-          <Text style={styles.textoTitulo}>Restaurantes</Text>
-          <Text style={styles.textoTitulo2}>Hotel</Text>
+          <Text style={styles.textoTitulo}>Populares da semana</Text>
 
           <View style={styles.cardContainer}>
-            {publicacoes.map((pub, index) => (
+            {publicacoes.map((publicacao, index) => (
               <TouchableOpacity key={index} style={styles.card} onPress={() => router.push('/CardDetalhes')}>
-                {pub.imagemUrl ? (
-                  <Image source={{ uri: pub.imagemUrl }} style={styles.cardImage} />
-                ) : (
-                  <View style={styles.noImage}>
-                    <Text style={styles.noImageText}>Sem Imagem</Text>
-                  </View>
-                )}
-                <Text style={styles.cardTitle}>{pub.titulo}</Text>
-                <Text style={styles.cardDescription}>{pub.descricao}</Text>
+                <Image source={{ uri: publicacao.imageUrl }} style={styles.cardImage} />
+                <Text style={styles.cardTitle}>{publicacao.title}</Text>
+                <Text style={styles.cardDescription}>{publicacao.description}</Text>
               </TouchableOpacity>
             ))}
-
-            <Text style={styles.textoTitulo3}>Restaurantes próximos de você</Text>
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.carouselContainer}>
-              <TouchableOpacity style={styles.cardCarousel}>
-                <Image source={require('../assets/LePetit.png')} style={styles.cardImageCarrossel} />
-                <Text style={styles.cardTitle}>Lê Petit</Text>
-                <Text style={styles.cardDescription}>A cidade da luz e do amor.</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.cardCarousel}>
-                <Image source={require('../assets/LePetit.png')} style={styles.cardImageCarrossel} />
-                <Text style={styles.cardTitle}>Lê Petit</Text>
-                <Text style={styles.cardDescription}>Coração tecnológico do Japão.</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.cardCarousel}>
-                <Image source={require('../assets/LePetit.png')} style={styles.cardImageCarrossel} />
-                <Text style={styles.cardTitle}>Lê Petit</Text>
-                <Text style={styles.cardDescription}>0,3 km.</Text>
-              </TouchableOpacity>
-            </ScrollView>
           </View>
+
+          <Text style={styles.textoTitulo3}>Restaurantes próximos de você</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.carouselContainer}>
+            {publicacoes.filter(pub => pub.category === 'Restaurante').map((publicacao, index) => (
+              <TouchableOpacity key={index} style={styles.cardCarousel}>
+                <Image source={{ uri: publicacao.imageUrl }} style={styles.cardImageCarrossel} />
+                <Text style={styles.cardTitle}>{publicacao.title}</Text>
+                <Text style={styles.cardDescription}>{publicacao.description}</Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
         </View>
       </ScrollView>
-      <BottomNavBar openSearchModal={openSearchModal} />
+
+      <BottomNavBar openSearchModal={() => setModalVisible(true)} />
 
       <Modal
         animationType="slide"
@@ -154,6 +127,7 @@ const Explorar = () => {
 };
 
 const styles = StyleSheet.create({
+  // Aqui estão as definições de estilo, iguais às que você forneceu
   mainContainer: {
     flex: 1,
     backgroundColor: '#FFFFFF',
@@ -168,7 +142,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
   },
   inputContainer: {
-    height: '23%',
+    height: '15%', // aqui
     paddingTop: 0,
     width: '100%',
     backgroundColor: '#2D9AFF',
@@ -269,13 +243,17 @@ const styles = StyleSheet.create({
     width: '50%',
     backgroundColor: '#FFFFFF',
     borderRadius: 10,
-    padding: 20,
-    alignItems: 'center',
+    padding: 10,
     marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
   },
   cardImage: {
-    width: 170,
-    height: 100,
+    width: '100%',
+    height: 120,
     borderRadius: 10,
     marginBottom: 10,
   },
@@ -285,22 +263,30 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   cardDescription: {
-    textAlign: 'center',
+    fontSize: 14,
+    color: '#555',
   },
-  cardImageCarrossel: {
-    width: 160,
-    height: 100,
-    borderRadius: 10,
-    marginBottom: 10,
+  carouselContainer: {
+    marginTop: 20,
+    bottom: 50,
   },
   cardCarousel: {
-    width: 170,
+    width: 150,
     backgroundColor: '#FFFFFF',
     borderRadius: 10,
-    padding: 20,
-    alignItems: 'center',
-    marginRight: 10,
-    marginLeft: 10,
+    padding: 10,
+    marginRight: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  cardImageCarrossel: {
+    width: '100%',
+    height: 120,
+    borderRadius: 10,
+    marginBottom: 10,
   },
   modalContainer: {
     flex: 1,
@@ -310,59 +296,36 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
     padding: 20,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  searchInput: {
-    width: '100%',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#007BFF',
-    borderRadius: 10,
-    marginBottom: 20,
-    backgroundColor: '#FFFFFF',
-    color: '#333',
-  },
-  searchButton: {
-    backgroundColor: '#007BFF',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  searchButtonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  closeButton: {
-    backgroundColor: '#007BFF',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 20,
-    top:13,
-  },
-  closeButtonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
   },
   configOption: {
-    width: '100%',
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#DDD',
     flexDirection: 'row',
     alignItems: 'center',
-  },
-  configOptionText: {
-    fontSize: 16,
-    marginLeft: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f0f0f0',
   },
   configIcon: {
     marginRight: 10,
+  },
+  configOptionText: {
+    fontSize: 16,
+    color: '#000',
+  },
+  closeButton: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  closeButtonText: {
+    fontSize: 18,
+    color: '#007BFF',
   },
 });
 
